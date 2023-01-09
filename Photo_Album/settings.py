b9cde53f317f64ce4264d26593793d0efea6727d
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-01-09 14:52:56 +0200

    it works again

--- Photo_Album/settings.py
+++ Photo_Album/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
 ALLOWED_HOSTS = [
     'localhost',
